968a5927d443cdfcc61f59398329f603ef1c48e4
Finley-11 <118800133+Finley-11@users.noreply.github.com>
2023-01-17 08:52:40 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+let Temperatura = 0
 basic.forever(function () {
-	
+    basic.showNumber(Temperatura)
+})
+basic.forever(function () {
+    Temperatura = input.temperature()
+})
+basic.forever(function () {
+    if (7 < Temperatura) {
+        music.playTone(255, music.beat(BeatFraction.Breve))
+        music.playTone(255, music.beat(BeatFraction.Breve))
+        music.playTone(255, music.beat(BeatFraction.Breve))
+        music.playTone(255, music.beat(BeatFraction.Breve))
+    }
 })
